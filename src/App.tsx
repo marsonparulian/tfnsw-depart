1d531c708b901876661b238cc4efdd24f7d8c8c7
marson parulian <marson.parulian@gmail.com>
2021-04-27 14:07:33 +1000

    init: component to show list of departure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import DepartureList from "./components/departure-list";
 import './App.css';
 
-function App() {
+function App() {  
   return (
     <div className="App">
       <h1>Departure board for NSW - Australia</h1>
       <header className="App-header">
       </header>
+      <div className="list departure-list">
+        <DepartureList />
+      </div>
     </div>
   );
 }
---
+++ src/components/departure-list.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+/**
+ * Component to show list of departures
+*/
+export const DepartureList: React.FC = () => {
+
+    // Render
+    return (
+        <div>
+            This is list of departure
+        </div>
+    )
+}
+export default DepartureList;
