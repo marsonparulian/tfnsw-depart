d038c11a4a9f3796254fce3d3b6c6ec6e68d12fc
marson parulian <marson.parulian@gmail.com>
2021-04-29 22:47:21 +1000

    init: component to search & select stop

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { IDepartureMonResponse, IStopEvent } from "./type";
 import tripPlannerAPI from "./apis/tripPlanner";
+import StopSelect from "./components/stop-select";
 import DepartureList from "./components/departure-list";
 import './App.css';
 import { AxiosResponse } from 'axios';
@@ -24,6 +25,9 @@ function App() {
   return (
     <div className="App">
       <header><h1>Departure board for NSW - Australia</h1></header>
+      <div className="departure-select-cont">
+        <StopSelect />
+      </div>
       <div className="list departure-list-cont">
         <DepartureList
           stopEvents={stopEvents}
---
+++ src/components/stop-select.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+/**
+ * Component to search & select stop
+ */
+const StopSelect: React.FC = () => {
+
+    // Render
+    return (
+        <div className="stop-select">
+            this is the stop seelect
+        </div>
+    )
+}
+
+export default StopSelect;
